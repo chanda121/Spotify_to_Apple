4479baf858802494ebff08d03d0728f8be1ab392
Danny <dannychan.cmw@gmail.com>
2026-03-29 12:50:38 -0500

    Added token check in backend

--- backend/src/routes/spotify/player.ts
+++ backend/src/routes/spotify/player.ts
@@ -1,10 +1,10 @@
 import express from 'express'
 import { getCurrentlyPlaying } from '../../services/spotify/playerService.js'
-import { requireAuth } from '../../middleware/requireAuth.js'
+import { requireSpotifyAuth } from '../../middleware/requireAuth.js'
 
 
 const router = express.Router()
 
-router.get('/get-current-track', requireAuth, getCurrentlyPlaying)
+router.get('/get-current-track', requireSpotifyAuth, getCurrentlyPlaying)
 
 export default router
